e1f53cf1278a823b743c97cc3520ef19385e04c4
Brijesh Tadhani <btadhani2412@gmail.com>
2025-12-12 14:22:30 +0530

    Other Files

--- backend/carMech/carMech/urls.py
+++ backend/carMech/carMech/urls.py
@@ -19,9 +19,13 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/",include('car.urls')),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
